96dd422960fe4a99c6fb68823942b0201d7fe670
ashwinxprasad <ashwinxprasad@gmail.com>
2020-09-25 16:16:55 +0530

    added jekyll files

---
+++ assets/js/lunrsearchengine.js
@@ -0,0 +1,129 @@
+
+var documents = [{
+    "id": 0,
+    "url": "/404.html",
+    "title": "404",
+    "body": "404 Page not found!Please use the search bar from the bottom left or visit our homepage! "
+    }, {
+    "id": 1,
+    "url": "/privacy-policy",
+    "title": "Privacy Policy for UnGrad",
+    "body": "At UnGrad, accessible from https://ungrad. tk, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by UnGrad and how we use it. If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us. This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in UnGrad. This policy is not applicable to any information collected offline or via channels other than this website. Our Privacy Policy was created with the help of the Privacy Policy Generator and the Free Privacy Policy Generator. Consent: By using our website, you hereby consent to our Privacy Policy and agree to its terms. Information we collect: The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information. If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide. When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number. How we use your information: We use the information we collect in various ways, including to: Provide, operate, and maintain our websteImprove, personalize, and expand our websteUnderstand and analyze how you use our websteDevelop new products, services, features, and functionalityCommunicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the webste, and for marketing and promotional purposesSend you emailsFind and prevent fraudLog Files: UnGrad follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information. Cookies and Web Beacons: Like any other website, UnGrad uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information. For more general information on cookies, please read  What Are Cookies . Google DoubleClick DART Cookie: Google is one of a third-party vendor on our site. It also uses cookies, known as DART cookies, to serve ads to our site visitors based upon their visit to www. website. com and other sites on the internet. However, visitors may choose to decline the use of DART cookies by visiting the Google ad and content network Privacy Policy at the following URL – https://policies. google. com/technologies/ads Advertising Partners Privacy Policies: You may consult this list to find the Privacy Policy for each of the advertising partners of UnGrad. Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on UnGrad, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit. Note that UnGrad has no access to or control over these cookies that are used by third-party advertisers. Third Party Privacy Policies: UnGrad's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options.  You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites. CCPA Privacy Rights (Do Not Sell My Personal Information): Under the CCPA, among other rights, California consumers have the right to: Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers. Request that a business delete any personal data about the consumer that a business has collected. Request that a business that sells a consumer's personal data, not sell the consumer's personal data. If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us. GDPR Data Protection Rights: We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following: The right to access – You have the right to request copies of your personal data. We may charge you a small fee for this service. The right to rectification – You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete the information you believe is incomplete. The right to erasure – You have the right to request that we erase your personal data, under certain conditions. The right to restrict processing – You have the right to request that we restrict the processing of your personal data, under certain conditions. The right to object to processing – You have the right to object to our processing of your personal data, under certain conditions. The right to data portability – You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions. If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us. Children's Information: Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity. UnGrad does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records. "
+    }, {
+    "id": 2,
+    "url": "/categories",
+    "title": "Categories",
+    "body": ""
+    }, {
+    "id": 3,
+    "url": "/contact",
+    "title": "Contact",
+    "body": "  Please send your message to UnGrad. We will reply as soon as possible!   "
+    }, {
+    "id": 4,
+    "url": "/",
+    "title": "Home",
+    "body": "                                Transfer Learning Deep Dive              1 2 3 4 5           :        PyTorch is quickly becoming one of the most popular deep learning frameworks around, as well as a must-have skill in your artificial intelligence tool kit. It’s gained admiration from industry. . .    :                     Rs.        6599        Now. Rs. 0 &lt;/span                05 Jan 2020           &lt;/div&gt;  &lt;/div&gt; &lt;/div&gt;&lt;/div&gt;                                Text analytics and predictions using Python              1 2 3 4 5           :        Text is a rich source of insights for businesses. Websites, social media, emails, and chats all contain valuable customer data. But to reap the rewards, you need to be able. . .    :                     Rs.        6599        Now. Rs. 0 &lt;/span                03 Feb 2020           &lt;/div&gt;  &lt;/div&gt; &lt;/div&gt;&lt;/div&gt;                                Building a recommendation system with Python ML              1 2 3 4 5           :        Discover how to use Python—and some essential machine learning concepts—to build programs that can make recommendations. In this hands-on course, our instructor covers the different types of recommendation systems out. . .    :                     Rs.        4599        Now. Rs. 0 &lt;/span                03 Feb 2020           &lt;/div&gt;  &lt;/div&gt; &lt;/div&gt;&lt;/div&gt;                                Python for data-science (Part 2)              1 2 3 4 5           :        In this course, the instructor takes you step by step through a practical data science project: building machine learning models that can generate predictions and recommendations and automate routine tasks. . . .    :                     Rs.        5599        Now. Rs. 0 &lt;/span                30 Apr 2020           &lt;/div&gt;  &lt;/div&gt; &lt;/div&gt;&lt;/div&gt;                                Python for data-science (Part 1)              1 2 3 4 5           :        Python for Data Science Essential Training is one of the most popular data science courses at UnGrad Learning. It has now been updated and expanded to two parts—for even more. . .    :                     Rs.        4599        Now. Rs. 0 &lt;/span                25 Mar 2020           &lt;/div&gt;  &lt;/div&gt; &lt;/div&gt;&lt;/div&gt;                                Amazon EC2 Deep Dive              1 2 3 4 5           :        Amazon Elastic Compute Cloud (EC2) provides secure, resizable compute capacity in the cloud. You can quickly increase or decrease capacity as your requirements change—only paying for the capacity you need. . .    :                     Rs.        3599        Now. Rs. 0 &lt;/span                13 Aug 2020           &lt;/div&gt;  &lt;/div&gt; &lt;/div&gt;&lt;/div&gt;  &lt;/div&gt;  "
+    }, {
+    "id": 5,
+    "url": "/robots.txt",
+    "title": "",
+    "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
+    }, {
+    "id": 6,
+    "url": "/transfer-learning-deep-dive/",
+    "title": "Transfer Learning Deep Dive",
+    "body": "2020/09/25 - PyTorch is quickly becoming one of the most popular deep learning frameworks around, as well as a must-have skill in your artificial intelligence tool kit. It’s gained admiration from industry leaders due to its deep integration with Python; its integration with top cloud platforms, including Amazon SageMaker and Google Cloud Platform; and its computational graphs that can be defined on the fly. In this course, join our instructor as he dives into the basics of deep learning using PyTorch. Starting with a working image recognition model, he shows how the different components fit and work in tandem—from tensors, loss functions, and autograd all the way to troubleshooting a PyTorch network. "
+    }, {
+    "id": 7,
+    "url": "/text-analytics/",
+    "title": "Text analytics and predictions using Python",
+    "body": "2020/09/25 - Text is a rich source of insights for businesses. Websites, social media, emails, and chats all contain valuable customer data. But to reap the rewards, you need to be able to analyze large amounts of unstructured text. Text mining is an essential skill for anyone working in big data and data science. This course teaches text-mining techniques to extract, cleanse, and process text using Python and the scikit-learn and nltk libraries. Our Indian instructor explains how to perform text analytics using popular techniques like word cloud and sentiment analysis. He then shows how to make predictions with text data using clustering, classification, and recommendations—otherwise known as predictive text. Along the way, he introduces important text analytics concepts such as lemmatization and n-grams. "
+    }, {
+    "id": 8,
+    "url": "/recommendation-system/",
+    "title": "Building a recommendation system with Python ML",
+    "body": "2020/09/25 - Discover how to use Python—and some essential machine learning concepts—to build programs that can make recommendations. In this hands-on course, our instructor covers the different types of recommendation systems out there, and shows how to build each one. She helps you learn the concepts behind how recommendation systems work by taking you through a series of examples and exercises. Once you’re familiar with the underlying concepts, Lillian explains how to apply statistical and machine learning methods to construct your own recommenders. She demonstrates how to build a popularity-based recommender using the Pandas library, how to recommend similar items based on correlation, and how to deploy various machine learning algorithms to make recommendations. At the end of the course, she shows how to evaluate which recommender performed the best. "
+    }, {
+    "id": 9,
+    "url": "/python-data-science-pt2/",
+    "title": "Python for data-science (Part 2)",
+    "body": "2020/09/25 - In this course, the instructor takes you step by step through a practical data science project: building machine learning models that can generate predictions and recommendations and automate routine tasks. Along the way, she shows how to perform linear and logistic regression, use K-means and hierarchal clustering, identify relationships between variables, and use other machine learning tools such as neural networks and Bayesian models. You should walk away from this training with hands-on coding experience that you can quickly apply to your own data science projects. "
+    }, {
+    "id": 10,
+    "url": "/python-data-science-pt1/",
+    "title": "Python for data-science (Part 1)",
+    "body": "2020/09/25 -  Python for Data Science Essential Training is one of the most popular data science courses at UnGrad Learning. It has now been updated and expanded to two parts—for even more hands-on experience with Python. In this course, instructor takes you step by step through a practical data science project: a web scraper that downloads and analyzes data from the web. Along the way, she introduces techniques to clean, reformat, transform, and describe raw data; generate visualizations; remove outliers; perform simple data analysis; and generate interactive graphs using the Plotly library. You should walk away from this training with basic coding experience that you can take to your organization and quickly apply to your own custom data science projects. . "
+    }, {
+    "id": 11,
+    "url": "/amazon-ec2/",
+    "title": "Amazon EC2 Deep Dive",
+    "body": "2020/09/25 - Amazon Elastic Compute Cloud (EC2) provides secure, resizable compute capacity in the cloud. You can quickly increase or decrease capacity as your requirements change—only paying for the capacity you need now. In this course, instructor Shyam Raj helps you deepen your understanding of Amazon EC2. Learn how to attach Elastic Block Store (EBS) volumes to your instances and enable features such as encryption and snapshots, which help you back up the data on your volumes. Shyam also provides coverage of Amazon Machine Images (AMI) and explores the key features, working mechanism, and use cases for T2 and T3 instances. Plus, discover the options for Amazon EC2 networking and explore purchasing options, such as on-demand versus reserved and spot instances, that help you maximize your budget without sacrificing power. "
+    }];
+
+var idx = lunr(function () {
+    this.ref('id')
+    this.field('title')
+    this.field('body')
+
+    documents.forEach(function (doc) {
+        this.add(doc)
+    }, this)
+});
+function lunr_search(term) {
+    document.getElementById('lunrsearchresults').innerHTML = '<ul></ul>';
+    if(term) {
+        document.getElementById('lunrsearchresults').innerHTML = "<p>Search results for '" + term + "'</p>" + document.getElementById('lunrsearchresults').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><span class='body'>"+ body +"</span><span class='url'>"+ url +"</span></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>No results found...</li>";
+        }
+    }
+    return false;
+}
+
+function lunr_search(term) {
+    $('#lunrsearchresults').show( 400 );
+    $( "body" ).addClass( "modal-open" );
+    
+    document.getElementById('lunrsearchresults').innerHTML = '<div id="resultsmodal" class="modal fade show d-block"  tabindex="-1" role="dialog" aria-labelledby="resultsmodal"> <div class="modal-dialog shadow" role="document"> <div class="modal-content"> <div class="modal-header" id="modtit"> <button type="button" class="close" id="btnx" data-dismiss="modal" aria-label="Close"> &times; </button> </div> <div class="modal-body"> <ul class="mb-0"> </ul>    </div> <div class="modal-footer"><button id="btnx" type="button" class="btn btn-primary btn-sm" data-dismiss="modal">Close</button></div></div> </div></div>';
+    if(term) {
+        document.getElementById('modtit').innerHTML = "<h5 class='modal-title'>Search results for '" + term + "'</h5>" + document.getElementById('modtit').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><small><span class='body'>"+ body +"</span><span class='url'>"+ url +"</span></small></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>Sorry, no results found. Close & try a different search!</li>";
+        }
+    }
+    return false;
+}
+    
+$(function() {
+    $("#lunrsearchresults").on('click', '#btnx', function () {
+        $('#lunrsearchresults').hide( 5 );
+        $( "body" ).removeClass( "modal-open" );
+    });
+});
